8b1374793b55aa6f95cb95c84a29f7c3dc49e843
Андрей <andhunter77@gmail.com>
2024-07-10 14:27:34 +0500

    Досмотрел 1 курс смотрю другой

--- mydjangoproject/myapp1/views.py
+++ mydjangoproject/myapp1/views.py
@@ -3,8 +3,5 @@ from myapp1.models import Worker
 
 
 def index_page(request):
-    worker_to_change = Worker.objects.get(id=5)
     all_workers = Worker.objects.all()
-    for i in all_workers:
-        print(f'Фамилия: {i.second_name}, Имя: {i.name}, Зарплата: {i.salary}, ID: {i.id} ')
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={'data': all_workers})
