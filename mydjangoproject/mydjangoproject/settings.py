27043a84c6bfe9bb03425c2a9fd65a7c571c0a46
Андрей <andhunter77@gmail.com>
2024-07-12 14:21:18 +0500

    разобраться с отображением фото

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -118,6 +118,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp1/static/img_dir/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
